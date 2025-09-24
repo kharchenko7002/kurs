34132547e6dbad62746ac645b9ba7c49f298e02c
kharchenko7002 <kharchenko7002@gmail.com>
2025-09-24 12:19:19 +0200

    временный коммит. чтобы потом удалить

---
+++ app/client/show/layout.js
@@ -0,0 +1,19 @@
+import { Poppins } from "next/font/google";
+import "./style.css";
+
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ["latin"],
+});
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <h1>info</h1>
+        {children}
+      </body>
+    </html>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,28 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <h1>Главная страница</h1>
+    <main className="container">
+      <h1 className="title">min portfolio</h1>
+
+      {/* Фото */}
+      <div className="photo">
+        <Image
+          src="/me.jpg"   // путь внутри public/
+          alt="Моё фото"
+          width={250}     // ширина
+          height={250}    // высота
+          className="profile-pic"
+        />
+      </div>
+
+      {/* Карточки */}
+      <div className="cards">
+        <div className="card">Mine prosjekter</div>
+        <div className="card">Min CV</div>
+        <div className="card">Min utdanning</div>
+        <div className="card">Min arbeidserfaring</div>
+      </div>
+    </main>
   );
 }
