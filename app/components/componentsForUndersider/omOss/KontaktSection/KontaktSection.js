6b9591fefa23135850e00cf02d63989cad675ae2
kharchenko7002 <kharchenko7002@gmail.com>
2025-10-27 11:47:46 +0100

    123

--- app/components/componentsForUndersider/omOss/KontaktSection/KontaktSection.js
+++ app/components/componentsForUndersider/omOss/KontaktSection/KontaktSection.js
@@ -1,5 +1,5 @@
-import Kundeservice from "../Kundeservice/Kundeservice";
-import KontaktInfo from "../Kontaktinfo/KontaktInfo";
+import Kundeservice from "../Kundeservice/Kundeservice.js";
+import KontaktInfo from "../Kontaktinfo/KontaktInfo.js";
 
 export default function KontaktSection() {
   return (
